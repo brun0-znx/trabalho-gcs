4b97187a8722de5fc224e38e73647d4b5d75ef3a
brun0-znx <bruno.chanan01@edu.pucrs.br>
2021-05-21 19:53:19 -0300

    cadastro de médico

--- src/Sistema.java
+++ src/Sistema.java
@@ -11,4 +11,26 @@ public class Sistema {
     public Sistema() {
 
     }
+    /*
+    Funções de administrador
+     */
+    public void cadastraMédico(){
+        Scanner teclado = new Scanner(System.in);
+        int id;
+        String nome;
+
+        System.out.println("Informe o ID do médico: \n");
+        id = teclado.nextInt();
+
+        System.out.println("Informe o nome do médico: \n");
+        nome = teclado.next();
+
+        Medico medico1 = new Medico(id,nome);
+        setMedicos(medico1);
+    }
+
+    public void setMedicos(Medico umMedico) {
+        medicos.add(umMedico);
+        System.out.println("Médico cadastrado!");
+    }
 }
